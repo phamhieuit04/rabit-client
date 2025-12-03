09c3b54844d5367cb45d46fcd2656ea23881622e
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-12-03 21:42:53 +0700

    Add footer + update locales

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex min-h-80 w-full justify-center bg-[#3d3c3c] p-12">
+        <div class="container mx-auto grid w-full max-w-7xl grid-cols-4 gap-6 text-white">
+            <div class="flex flex-col gap-3">
+                <p>{{ $t('footer.slogan') }}</p>
+                <h1 class="text-xl font-bold uppercase">Rabit Notebuck</h1>
+            </div>
+            <div>
+                <h1 class="pb-4 text-xl font-medium" style="font-family: 'Cormorant Garamond'">
+                    {{ $t('footer.about_crabit') }}
+                </h1>
+                <ul class="flex flex-col gap-1">
+                    <li>{{ $t('footer.intro') }}</li>
+                    <li>{{ $t('footer.product_advantages') }}</li>
+                    <li>{{ $t('footer.inspiration') }}</li>
+                </ul>
+            </div>
+            <div>
+                <h1 class="pb-4 text-xl font-medium" style="font-family: 'Cormorant Garamond'">
+                    {{ $t('footer.customer_policy') }}
+                </h1>
+                <ul class="flex flex-col gap-1">
+                    <li>{{ $t('footer.bulk_order') }}</li>
+                    <li>{{ $t('footer.personal_order') }}</li>
+                </ul>
+            </div>
+            <div>
+                <h1 class="pb-4 text-xl font-medium" style="font-family: 'Cormorant Garamond'">
+                    {{ $t('footer.newsletter') }}
+                </h1>
+                <p>{{ $t('footer.newsletter_desc') }}</p>
+            </div>
+        </div>
+    </div>
+</template>
--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,8 +1,23 @@
+import type { ILanguage } from '@/interfaces/ILanguage'
+
 export const en: ILanguage = {
     header: {
         shop: 'Shop',
         contact: 'Contact',
         home: 'Home',
+    },
+    footer: {
+        slogan: 'Made in Vietnam notebooks for young and passionate people!',
+        about_crabit: 'About Crabit',
+        intro: 'General introduction',
+        product_advantages: 'Product advantages',
+        inspiration: 'Crabit Notebook inspiration',
+        customer_policy: 'Customer policy',
+        bulk_order: 'Bulk order notebooks',
+        personal_order: 'Personalized notebooks',
+        newsletter: 'Newsletter subscription',
+        newsletter_desc:
+            'Subscribe to be the first to know about new products and exclusive offers!',
     },
     auth: {
         login: 'Log in',
