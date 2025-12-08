162575931417e5c69d53456312a28091edb44849
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2025-12-08 14:17:06 +0700

    Revert "Feature/products page"

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    scrollBehavior(to, from, savedPosition) {
-        return {
-            top: 0,
-        }
-    },
     routes: [
         {
             path: '/',
             name: 'home',
             component: () => import('../views/HomeView.vue'),
-        },
-        {
-            path: '/products',
-            name: 'products',
-            component: () => import('../views/ProductsView.vue'),
         },
     ],
 })
--- src/views/ProductsView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import ListProductComponent from '@/components/ListProductComponent.vue'
-</script>
-
-<template>
-    <div class="container mx-auto flex max-w-7xl flex-col items-center justify-center">
-        <ListProductComponent />
-    </div>
-</template>
