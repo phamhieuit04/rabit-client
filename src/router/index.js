17282e57754fa24559ce49d6ec2bd6cc298d1929
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-12-08 21:44:03 +0700

    Revert all missing content

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, from, savedPosition) {
+        return {
+            top: 0,
+        }
+    },
     routes: [
         {
             path: '/',
             name: 'home',
             component: () => import('../views/HomeView.vue'),
+        },
+        {
+            path: '/products',
+            name: 'products',
+            component: () => import('../views/ProductsView.vue'),
         },
         {
             path: '/register',
