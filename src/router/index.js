44cde3ed0b3abf5ac15d7399000de0c28ab46c7d
HieuPNT <79359537+phamhieuit04@users.noreply.github.com>
2025-12-08 15:13:51 +0700

    Revert "Feature/auth intergration"

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,6 @@ const router = createRouter({
             name: 'home',
             component: () => import('../views/HomeView.vue'),
         },
-        {
-            path: '/register',
-            name: 'register',
-            component: () => import('../views/RegisterView.vue'),
-        },
     ],
 })
 
